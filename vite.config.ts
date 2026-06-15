06bfb080e419215fcef7224699f7a26ae94e46fc
zj <arbasgoat@gmail.com>
2026-06-15 10:52:43 +0800

    chore: add @shared alias to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'client/src'),
+      '@shared': path.resolve(__dirname, 'shared'),
     },
   },
   root: 'client',
